5d25a29bcd1f37efbda29a7077f945bca620bee2
Vedika365 <vedikajain873@yahoo.com>
2024-12-04 21:51:26 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,25 @@ public class Main {
     public static void main(String[] args) {
 
         //create ArrayList object
-
+        ArrayList<String> books;
         //default constructor
 
+        //need to add five different books
         public Main(){
-
+         books.add()
         }
 
+        //getter and setter
 
+        //getter for library
+        public ArrayList<String> getBooks(){
+            return books;
+        }
+
+        //setter for books
+        public void setBooks(ArrayList<String>books){
+            this.books = new ArrayList<>(books);
+        }
 
 
 
